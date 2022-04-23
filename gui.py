db0e6103ed7f3e753ff6e549285128643f00aa7e
kompostkey <komtos@seznam.cz>
2022-04-23 06:26:56 -0700

    Refatoring v GUI

--- gui.py
+++ gui.py
@@ -14,67 +14,67 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GButton_19=tk.Button(root)
-        GButton_19["bg"] = "#efefef"
+        GButton_start=tk.Button(root)
+        GButton_start["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_19["font"] = ft
-        GButton_19["fg"] = "#000000"
-        GButton_19["justify"] = "center"
-        GButton_19["text"] = "START"
-        GButton_19.place(x=10,y=10,width=112,height=41)
-        GButton_19["command"] = self.GButton_19_command
+        GButton_start["font"] = ft
+        GButton_start["fg"] = "#000000"
+        GButton_start["justify"] = "center"
+        GButton_start["text"] = "START"
+        GButton_start.place(x=10,y=10,width=112,height=41)
+        GButton_start["command"] = self.GButton_start_command
 
-        GButton_917=tk.Button(root)
-        GButton_917["bg"] = "#efefef"
+        GButton_stop=tk.Button(root)
+        GButton_stop["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_917["font"] = ft
-        GButton_917["fg"] = "#000000"
-        GButton_917["justify"] = "center"
-        GButton_917["text"] = "STOP"
-        GButton_917.place(x=10,y=70,width=112,height=41)
-        GButton_917["command"] = self.GButton_917_command
+        GButton_stop["font"] = ft
+        GButton_stop["fg"] = "#000000"
+        GButton_stop["justify"] = "center"
+        GButton_stop["text"] = "STOP"
+        GButton_stop.place(x=10,y=70,width=112,height=41)
+        GButton_stop["command"] = self.GButton_stop_command
 
-        GLabel_889=tk.Label(root)
+        GLabel_cas=tk.Label(root)
         ft = tkFont.Font(family='Times',size=12)
-        GLabel_889["font"] = ft
-        GLabel_889["fg"] = "#333333"
-        GLabel_889["justify"] = "left"
-        GLabel_889["text"] = "Čas:"
-        GLabel_889.place(x=10,y=120,width=54,height=41)
+        GLabel_cas["font"] = ft
+        GLabel_cas["fg"] = "#333333"
+        GLabel_cas["justify"] = "left"
+        GLabel_cas["text"] = "Čas:"
+        GLabel_cas.place(x=10,y=120,width=54,height=41)
 
-        GLabel_695=tk.Label(root)
+        GLabel_zachyceno=tk.Label(root)
         ft = tkFont.Font(family='Times',size=12)
-        GLabel_695["font"] = ft
-        GLabel_695["fg"] = "#333333"
-        GLabel_695["justify"] = "left"
-        GLabel_695["text"] = "Zachyceno paketů:"
-        GLabel_695.place(x=5,y=160,width=150,height=43)
+        GLabel_zachyceno["font"] = ft
+        GLabel_zachyceno["fg"] = "#333333"
+        GLabel_zachyceno["justify"] = "left"
+        GLabel_zachyceno["text"] = "Zachyceno paketů:"
+        GLabel_zachyceno.place(x=5,y=160,width=150,height=43)
 
-        GLabel_895=tk.Label(root)
+        GLabel_cas_text=tk.Label(root)
         ft = tkFont.Font(family='Times',size=12)
-        GLabel_895["font"] = ft
-        GLabel_895["fg"] = "#333333"
-        GLabel_895["justify"] = "left"
-        GLabel_895["text"] = "0:2:36"
-        GLabel_895.place(x=160,y=120,width=72,height=41)
+        GLabel_cas_text["font"] = ft
+        GLabel_cas_text["fg"] = "#333333"
+        GLabel_cas_text["justify"] = "left"
+        GLabel_cas_text["text"] = "0:2:36"
+        GLabel_cas_text.place(x=160,y=120,width=72,height=41)
 
-        GLabel_551=tk.Label(root)
+        GLabel_zachyceno_text=tk.Label(root)
         ft = tkFont.Font(family='Times',size=12)
-        GLabel_551["font"] = ft
-        GLabel_551["fg"] = "#333333"
-        GLabel_551["justify"] = "left"
-        GLabel_551["text"] = "5640"
-        GLabel_551.place(x=160,y=160,width=70,height=41)
+        GLabel_zachyceno_text["font"] = ft
+        GLabel_zachyceno_text["fg"] = "#333333"
+        GLabel_zachyceno_text["justify"] = "left"
+        GLabel_zachyceno_text["text"] = "5640"
+        GLabel_zachyceno_text.place(x=160,y=160,width=70,height=41)
 
-        GButton_278=tk.Button(root)
-        GButton_278["bg"] = "#efefef"
+        GButton_vysledky=tk.Button(root)
+        GButton_vysledky["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_278["font"] = ft
-        GButton_278["fg"] = "#000000"
-        GButton_278["justify"] = "center"
-        GButton_278["text"] = "Výsledky"
-        GButton_278.place(x=10,y=210,width=112,height=41)
-        GButton_278["command"] = self.GButton_278_command
+        GButton_vysledky["font"] = ft
+        GButton_vysledky["fg"] = "#000000"
+        GButton_vysledky["justify"] = "center"
+        GButton_vysledky["text"] = "Výsledky"
+        GButton_vysledky.place(x=10,y=210,width=112,height=41)
+        GButton_vysledky["command"] = self.GButton_vysledky_command
 
         GLabel_104=tk.Label(root)
         ft = tkFont.Font(family='Times',size=26)
@@ -94,23 +94,23 @@ class App:
         GLabel_754["relief"] = "flat"
         GLabel_754.place(x=290,y=70,width=260,height=42)
 
-        GLabel_631=tk.Label(root)
+        GLabel_pomer_dat_text=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_631["font"] = ft
-        GLabel_631["fg"] = "#333333"
-        GLabel_631["justify"] = "left"
-        GLabel_631["text"] = "86"
-        GLabel_631.place(x=570,y=70,width=57,height=42)
+        GLabel_pomer_dat_text["font"] = ft
+        GLabel_pomer_dat_text["fg"] = "#333333"
+        GLabel_pomer_dat_text["justify"] = "left"
+        GLabel_pomer_dat_text["text"] = "86"
+        GLabel_pomer_dat_text.place(x=570,y=70,width=57,height=42)
 
-        GButton_355=tk.Button(root)
-        GButton_355["bg"] = "#efefef"
+        GButton_graf_protokolu=tk.Button(root)
+        GButton_graf_protokolu["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=12)
-        GButton_355["font"] = ft
-        GButton_355["fg"] = "#000000"
-        GButton_355["justify"] = "center"
-        GButton_355["text"] = "Graf protokolu"
-        GButton_355.place(x=10,y=270,width=112,height=41)
-        GButton_355["command"] = self.GButton_355_command
+        GButton_graf_protokolu["font"] = ft
+        GButton_graf_protokolu["fg"] = "#000000"
+        GButton_graf_protokolu["justify"] = "center"
+        GButton_graf_protokolu["text"] = "Graf protokolu"
+        GButton_graf_protokolu.place(x=10,y=270,width=112,height=41)
+        GButton_graf_protokolu["command"] = self.GButton_graf_protokolu_command
 
         GLabel_19=tk.Label(root)
         GLabel_19["anchor"] = "center"
@@ -121,13 +121,13 @@ class App:
         GLabel_19["text"] = "Počet šifrovaných paketů:"
         GLabel_19.place(x=263,y=120,width=260,height=42)
 
-        GLabel_768=tk.Label(root)
+        GLabel_pocet_sif_pkt_text=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_768["font"] = ft
-        GLabel_768["fg"] = "#333333"
-        GLabel_768["justify"] = "left"
-        GLabel_768["text"] = "5098"
-        GLabel_768.place(x=570,y=120,width=57,height=42)
+        GLabel_pocet_sif_pkt_text["font"] = ft
+        GLabel_pocet_sif_pkt_text["fg"] = "#333333"
+        GLabel_pocet_sif_pkt_text["justify"] = "left"
+        GLabel_pocet_sif_pkt_text["text"] = "5098"
+        GLabel_pocet_sif_pkt_text.place(x=570,y=120,width=57,height=42)
 
         GLabel_648=tk.Label(root)
         GLabel_648["anchor"] = "center"
@@ -138,13 +138,13 @@ class App:
         GLabel_648["text"] = " Celkový objem šifrovaných dat [B]:"
         GLabel_648.place(x=295,y=170,width=260,height=42)
 
-        GLabel_622=tk.Label(root)
+        GLabel_objem_sif_dat_text=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_622["font"] = ft
-        GLabel_622["fg"] = "#333333"
-        GLabel_622["justify"] = "left"
-        GLabel_622["text"] = "3 456 543"
-        GLabel_622.place(x=570,y=170,width=100,height=42)
+        GLabel_objem_sif_dat_text["font"] = ft
+        GLabel_objem_sif_dat_text["fg"] = "#333333"
+        GLabel_objem_sif_dat_text["justify"] = "left"
+        GLabel_objem_sif_dat_text["text"] = "3 456 543"
+        GLabel_objem_sif_dat_text.place(x=570,y=170,width=100,height=42)
 
         GLabel_942=tk.Label(root)
         ft = tkFont.Font(family='Times',size=26)
@@ -162,21 +162,21 @@ class App:
         GLabel_248["text"] = "Změna počtu šifrovaných paketů:"
         GLabel_248.place(x=288,y=350,width=260,height=42)
 
-        GLabel_449=tk.Label(root)
+        GLabel_zmena_poct_sif_pkt_bool=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_449["font"] = ft
-        GLabel_449["fg"] = "#333333"
-        GLabel_449["justify"] = "left"
-        GLabel_449["text"] = "Ano"
-        GLabel_449.place(x=590,y=350,width=57,height=42)
+        GLabel_zmena_poct_sif_pkt_bool["font"] = ft
+        GLabel_zmena_poct_sif_pkt_bool["fg"] = "#333333"
+        GLabel_zmena_poct_sif_pkt_bool["justify"] = "left"
+        GLabel_zmena_poct_sif_pkt_bool["text"] = "Ano"
+        GLabel_zmena_poct_sif_pkt_bool.place(x=590,y=350,width=57,height=42)
 
-        GLabel_815=tk.Label(root)
+        GLabel_zmena_poc_sif_pkt_hodnota=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_815["font"] = ft
-        GLabel_815["fg"] = "#333333"
-        GLabel_815["justify"] = "left"
-        GLabel_815["text"] = "o 16 % více"
-        GLabel_815.place(x=670,y=350,width=100,height=42)
+        GLabel_zmena_poc_sif_pkt_hodnota["font"] = ft
+        GLabel_zmena_poc_sif_pkt_hodnota["fg"] = "#333333"
+        GLabel_zmena_poc_sif_pkt_hodnota["justify"] = "left"
+        GLabel_zmena_poc_sif_pkt_hodnota["text"] = "o 16 % více"
+        GLabel_zmena_poc_sif_pkt_hodnota.place(x=670,y=350,width=100,height=42)
 
         GLabel_180=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
@@ -186,36 +186,36 @@ class App:
         GLabel_180["text"] = "Změna objemu šifrovaných dát"
         GLabel_180.place(x=278,y=390,width=260,height=42)
 
-        GLabel_270=tk.Label(root)
+        GLabel_zmena_obj_sif_dat_bool=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_270["font"] = ft
-        GLabel_270["fg"] = "#333333"
-        GLabel_270["justify"] = "left"
-        GLabel_270["text"] = "Ano"
-        GLabel_270.place(x=590,y=390,width=57,height=42)
+        GLabel_zmena_obj_sif_dat_bool["font"] = ft
+        GLabel_zmena_obj_sif_dat_bool["fg"] = "#333333"
+        GLabel_zmena_obj_sif_dat_bool["justify"] = "left"
+        GLabel_zmena_obj_sif_dat_bool["text"] = "Ano"
+        GLabel_zmena_obj_sif_dat_bool.place(x=590,y=390,width=57,height=42)
 
-        GLabel_707=tk.Label(root)
+        GLabel_zmena_obj_sif_dat_hodnota=tk.Label(root)
         ft = tkFont.Font(family='Times',size=14)
-        GLabel_707["font"] = ft
-        GLabel_707["fg"] = "#333333"
-        GLabel_707["justify"] = "left"
-        GLabel_707["text"] = "o 24 % více"
-        GLabel_707.place(x=670,y=390,width=100,height=42)
+        GLabel_zmena_obj_sif_dat_hodnota["font"] = ft
+        GLabel_zmena_obj_sif_dat_hodnota["fg"] = "#333333"
+        GLabel_zmena_obj_sif_dat_hodnota["justify"] = "left"
+        GLabel_zmena_obj_sif_dat_hodnota["text"] = "o 24 % více"
+        GLabel_zmena_obj_sif_dat_hodnota.place(x=670,y=390,width=100,height=42)
 
-    def GButton_19_command(self):
-        print("command")
-
-
-    def GButton_917_command(self):
-        print("command")
+    def GButton_start_command(self):
+        print("Start...")
 
 
-    def GButton_278_command(self):
-        print("command")
+    def GButton_stop_command(self):
+        print("Stop...")
 
 
-    def GButton_355_command(self):
-        print("command")
+    def GButton_vysledky_command(self):
+        print("Vysledky...")
+
+
+    def GButton_graf_protokolu_command(self):
+        print("Graf protokolu...")
 
 if __name__ == "__main__":
     root = tk.Tk()
